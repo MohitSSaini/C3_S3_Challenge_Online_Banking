95926eb44f0db7d15a32e21ad317a36d0ff85077
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-26 21:50:45 +0530

    create a getter and setter method in SavingsAccount.java class

--- src/main/java/com/niit/jdp/SavingsAccount.java
+++ src/main/java/com/niit/jdp/SavingsAccount.java
@@ -20,7 +20,9 @@ public class SavingsAccount {
     
     }
     
-    public SavingsAccount ( String accountHolderName , long accountNumber , String accountOpeningDate , String accountStatus , double accountBalance , double totalBalance , boolean mobileBankingEnable , double interestRate , String modeOfOperation ) {
+    public SavingsAccount ( String accountHolderName , long accountNumber , String accountOpeningDate ,
+                            String accountStatus , double accountBalance , double totalBalance ,
+                            boolean mobileBankingEnable , double interestRate , String modeOfOperation ) {
         this.accountHolderName = accountHolderName;
         this.accountNumber = accountNumber;
         this.accountOpeningDate = accountOpeningDate;
@@ -29,6 +31,78 @@ public class SavingsAccount {
         this.totalBalance = totalBalance;
         this.mobileBankingEnable = mobileBankingEnable;
         this.interestRate = interestRate;
+        this.modeOfOperation = modeOfOperation;
+    }
+    
+    public String getAccountHolderName ( ) {
+        return accountHolderName;
+    }
+    
+    public void setAccountHolderName ( String accountHolderName ) {
+        this.accountHolderName = accountHolderName;
+    }
+    
+    public long getAccountNumber ( ) {
+        return accountNumber;
+    }
+    
+    public void setAccountNumber ( long accountNumber ) {
+        this.accountNumber = accountNumber;
+    }
+    
+    public String getAccountOpeningDate ( ) {
+        return accountOpeningDate;
+    }
+    
+    public void setAccountOpeningDate ( String accountOpeningDate ) {
+        this.accountOpeningDate = accountOpeningDate;
+    }
+    
+    public String getAccountStatus ( ) {
+        return accountStatus;
+    }
+    
+    public void setAccountStatus ( String accountStatus ) {
+        this.accountStatus = accountStatus;
+    }
+    
+    public double getAccountBalance ( ) {
+        return accountBalance;
+    }
+    
+    public void setAccountBalance ( double accountBalance ) {
+        this.accountBalance = accountBalance;
+    }
+    
+    public double getTotalBalance ( ) {
+        return totalBalance;
+    }
+    
+    public void setTotalBalance ( double totalBalance ) {
+        this.totalBalance = totalBalance;
+    }
+    
+    public boolean isMobileBankingEnable ( ) {
+        return mobileBankingEnable;
+    }
+    
+    public void setMobileBankingEnable ( boolean mobileBankingEnable ) {
+        this.mobileBankingEnable = mobileBankingEnable;
+    }
+    
+    public double getInterestRate ( ) {
+        return interestRate;
+    }
+    
+    public void setInterestRate ( double interestRate ) {
+        this.interestRate = interestRate;
+    }
+    
+    public String getModeOfOperation ( ) {
+        return modeOfOperation;
+    }
+    
+    public void setModeOfOperation ( String modeOfOperation ) {
         this.modeOfOperation = modeOfOperation;
     }
 }
